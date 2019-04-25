5c08c6fb54126d7e6646b8e467e586476d63c81e
Lincoln Ferreira Campos <link7-7@hotmail.com>
2019-04-25 00:12:35 -0300

    modelo de projeto basico

---
+++ WebApplicationCore/Projeto.WEB/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View(); //_repository.ListarProdutos()
+        }
+
+        public string Obter()
+        {
+            return "Feliz Ano Novo";
+        }
+    }
+}
--- WebApplicationCore/Projeto.WEB/Startup.cs
+++ WebApplicationCore/Projeto.WEB/Startup.cs
@@ -30,11 +30,13 @@ namespace Projeto.WEB
 
             /*Registro do servico, que tem a tarefa em segundo plano*/
             services.AddHostedService<SendingAMessageService>();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+        {/*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,7 +44,7 @@ namespace Projeto.WEB
             else
             {
                 app.UseHsts();
-            }
+            }*/
 
             app.UseHttpsRedirection();
             app.UseMvc();
